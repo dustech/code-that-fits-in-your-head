626d4890661245537f058803beedfb522a4aad56
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-11-06 23:54:02 +0100

    Guard against negative and zero quantity

---
+++ Restaurant.RestApi.Tests/ReservationTests.cs
@@ -0,0 +1,19 @@
+﻿using Dustech.Restaurant.RestApi.Models; // Reservation
+
+namespace Dustech.Restaurant.RestApi.Tests;
+
+public class ReservationTests
+{
+    [Theory(DisplayName = "QuantityMustBePositive")]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void QuantityMustBePositive(int invalidQantity)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => new Reservation(
+                new DateTime(2024, 8, 19, 11, 30, 0),
+                "mail@example.com",
+                "Tongolo Verace",
+                invalidQantity));
+    }
+}
--- Restaurant.RestApi/Models/Reservation.cs
+++ Restaurant.RestApi/Models/Reservation.cs
@@ -1,4 +1,10 @@
 namespace Dustech.Restaurant.RestApi.Models;
 
-public sealed record Reservation(DateTime At, string Email, string Name, int Quantity);
+public sealed record Reservation(DateTime At, string Email, string Name, int Quantity)
+{
+
+    public int Quantity { get; } = Quantity < 1 ?
+        throw new ArgumentOutOfRangeException(nameof(Quantity), "The value must be a positive (non-zero) number.") :
+        Quantity;
+}
 
