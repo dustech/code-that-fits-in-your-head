3307cda49280e2409993a60f63e9c221fe019992
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-11-08 18:31:09 +0100

    Aggiunto nello script la costruzione del report

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #lizard src/ -x"*/obj/*" -T cyclomatic_complexity=7 -w -W whitelizard.txt -l=csharp && \
 dotnet build && \
-dotnet test
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
