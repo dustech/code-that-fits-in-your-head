89e00eabeecf2f51145088cfc500afc5bda657e4
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-11-10 12:19:47 +0100

    Configure MaitreD with more than one table
    The main purpose of this change was to turn the unit test into a
    parametrised test, but it had the fortunate side-effect of already
    causing the change to MaîtreD.

--- src/Restaurant.RestApi.Tests/MaitreDTests.cs
+++ src/Restaurant.RestApi.Tests/MaitreDTests.cs
@@ -1,23 +1,34 @@
-﻿using System.Globalization;
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using Dustech.Restaurant.RestApi.Models; // MaitreD
 
 namespace Dustech.Restaurant.RestApi.Tests;
 
 public class MaitreDTests
 {
-    [Fact(DisplayName = "Accept")]
-    public void Accept()
-    {
-        MaitreD maitreD = new(new(TableType.Communal, 10));
 
-        Reservation sut = new(
+
+    [Theory(DisplayName = "Accept")]
+    [SuppressMessage("Performance",
+                    "CA1861: Avoid constant arrays as arguments",
+                    Justification = @"Nei test non e' rilevante
+                    la perdita di prestazioni")]
+    [InlineData(new int[] { 12 })]
+    public void Accept(int[] tableSeats)
+    {
+        var tables = tableSeats.Select(s => new Table(TableType.Communal, s));
+
+        MaitreD sut = new(tables);
+
+        Reservation r = new(
             At: DateTime.Parse("2023-11-08 18:00", CultureInfo.InvariantCulture),
             Email: "superpippo@ok.com",
             Name: "",
             Quantity: 1
         );
 
-        var willAccept = maitreD.WillAccept(Array.Empty<Reservation>(), sut);
+        var willAccept = sut.WillAccept(Array.Empty<Reservation>(), r);
 
         Assert.True(willAccept);
     }
--- src/Restaurant.RestApi/Models/MaitreD.cs
+++ src/Restaurant.RestApi/Models/MaitreD.cs
@@ -1,8 +1,21 @@
 ﻿
 namespace Dustech.Restaurant.RestApi.Models;
 
-public sealed class MaitreD(Table table)
+public sealed class MaitreD
 {
+    private readonly Table table;
+
+    public MaitreD(params Table[] tables)
+    {
+        ArgumentNullException.ThrowIfNull(tables);
+        this.table = tables[0];
+    }
+
+    public MaitreD(IEnumerable<Table> tables) : this(tables.ToArray())
+    {
+        ArgumentNullException.ThrowIfNull(tables);
+    }
+
     public bool WillAccept(
             IEnumerable<Reservation> existingReservations,
             Reservation candidate)
